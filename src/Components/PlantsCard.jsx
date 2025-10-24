7f4d8fb2e7fea08d9e5a4e86b5a68ffbeaeb6ac0
Reazul87 <hasanreazul87@gmail.com>
2025-10-24 07:06:29 -0300

    PLANTS_PAGE_COMPLETE

--- src/Components/PlantsCard.jsx
+++ src/Components/PlantsCard.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import { FaStar } from "react-icons/fa";
+import { Link } from "react-router";
 
-const PlantsCard = () => {
-          return (
-                    <div>
-                              
-                    </div>
-          );
+const PlantsCard = ({ plant }) => {
+  const { plantName, plantId, image, rating, price } = plant;
+
+  return (
+    <div className="card bg-base-300 shadow-sm">
+      <figure>
+        <img
+          src={image}
+          alt={plantName}
+          className="h-[22.5rem] w-full object-cover p-5"
+        />
+      </figure>
+      <div className="px-5 flex flex-col  gap-1.5 pb-5">
+        <h2 className="font-medium md:text-lg text-sm">{plantName}</h2>
+        <div className="flex justify-between font-medium">
+          <p>{price} $</p>
+          <p className="flex items-center gap-1.5">
+            <FaStar color="orange" size={20} /> {rating}
+          </p>
+        </div>
+        <Link
+          to={`/plants-details/${plantId}`}
+          className="btn btn-primary mt-1.5"
+        >
+          View Details
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PlantsCard;
--- src/Pages/Plants.jsx
+++ src/Pages/Plants.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useLoaderData } from "react-router";
+import PlantsCard from "../Components/PlantsCard";
+import MyContainer from "../Components/MyContainer";
+import { AuthContext } from "../Context/AuthContext";
+import Loading from "../Components/Loading";
+import { motion } from "framer-motion";
 
 const Plants = () => {
-          return (
-                    <div>
-                              
-                    </div>
-          );
+  const { loading } = useContext(AuthContext);
+  const plants = useLoaderData();
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  return (
+    <div className="bg-[#F5F5F0]">
+      <MyContainer className="px-5 md:px-20">
+        <motion.section
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+        >
+          <title>Plants</title>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:py-5 py-5">
+            {plants.map((plant, i) => (
+              <motion.div
+                key={plant.plantId}
+                initial={{ opacity: 0, scale: 0.9 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5, delay: i * 0.02 }}
+              >
+                <PlantsCard plant={plant}></PlantsCard>
+              </motion.div>
+            ))}
+          </div>
+        </motion.section>
+      </MyContainer>
+    </div>
+  );
 };
 
 export default Plants;
