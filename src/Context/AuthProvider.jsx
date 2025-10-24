a6ccdea74c29bc0275643bc537ba02b44342e92d
Reazul87 <hasanreazul87@gmail.com>
2025-10-24 06:55:44 -0300

    CONTEXT_API_COMPLETE

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import { auth } from "../Firebase/firebase.init";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-  const shared = {
-    password: 3545,
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // REGISTER
+  const registerUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
   };
-  return <AuthContext value={shared}>{children}</AuthContext>;
+  // LOGIN
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // LOGIN
+  const logout = () => {
+    return signOut(auth);
+  };
+  //GOOGLE
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+  // FORGET PASSWORD
+  const changePassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+  // UPDATE PROFILE
+  const profileUpdate = (displayName, photoURL) => {
+    return updateProfile(auth.currentUser, {
+      displayName,
+      photoURL,
+    });
+  };
+
+  // REMEMBER USER
+  useEffect(() => {
+    setLoading(true);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      return () => {
+        unsubscribe();
+      };
+    });
+  }, []);
+
+  const shared = {
+    user,
+    error,
+    login,
+    show,
+    logout,
+    setUser,
+    loading,
+    setError,
+    setShow,
+    setLoading,
+    registerUser,
+    googleLogin,
+    profileUpdate,
+    changePassword,
+  };
+  return <AuthContext.Provider value={shared}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
