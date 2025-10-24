d1ef3daaa098778469e6b9b98c70c3bf0312eea6
Reazul87 <hasanreazul87@gmail.com>
2025-10-24 07:11:18 -0300

    PLANTS_JSON_IMAGES_FOOTER_PAGE_COMPLETE

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,41 +1,37 @@
 import React from "react";
 import { Link } from "react-router";
 import MyContainer from "./MyContainer";
-
 const Footer = () => {
   return (
-    <div className="bg-neutral text-neutral p-8">
-      <MyContainer>
+    <div className="bg-[#16476A] text-neutral p-8">
+      <MyContainer className="px-5 md:px-20">
         <nav className="grid grid-cols-2 md:grid-cols-4 gap-5">
           <div className="flex flex-col">
-            <p className="text-gray-400 font-bold text-lg mb-2">Company</p>
+            <p className="text-white/75 font-bold text-lg mb-2">Company</p>
             <Link className="text-sm text-white/75">About us</Link>
             <Link className="text-sm text-white/75">Contact</Link>
             <Link className="text-sm text-white/75">Jobs</Link>
           </div>
-
           <div className="flex flex-col">
-            <p className="text-gray-400 font-bold text-lg mb-2">Legal</p>
+            <p className="text-white/75 font-bold text-lg mb-2">Legal</p>
             <Link className="text-sm text-white/75">Terms of use</Link>
             <Link className="text-sm text-white/75">Privacy policy</Link>
             <Link className="text-sm text-white/75">Cookie policy</Link>
           </div>
-
           <div className="flex flex-col">
-            <p className="text-gray-400 font-bold text-lg mb-2">Social</p>
+            <p className="text-white/75 font-bold text-lg mb-2">Social</p>
             <Link className="text-sm text-white/75">Pinterest</Link>
             <Link className="text-sm text-white/75">Facebook</Link>
             <Link className="text-sm text-white/75">Instagram</Link>
           </div>
-
           <div className="flex flex-col">
-            <p className="text-gray-400 font-bold text-lg mb-2">Apps</p>
+            <p className="text-white/75 font-bold text-lg mb-2">Apps</p>
             <Link className="text-sm text-white/75">Mac</Link>
             <Link className="text-sm text-white/75">iPhone</Link>
             <Link className="text-sm text-white/75">Windows</Link>
           </div>
         </nav>
-        <aside className="mt-5 px-4">
+        <aside className="mt-5 px-3">
           <p className="text-center text-sm font-medium text-white/80">
             Copyright - © {new Date().getFullYear()} GreenNest. All rights
             reserved
@@ -45,17 +41,4 @@ const Footer = () => {
     </div>
   );
 };
-
 export default Footer;
-
-{
-  /* Footer:
-
-Quick Links: About, Contact, Privacy Policy
-
-সোশ্যাল আইকন: Instagram, Facebook, Pinterest
-
-“© 2025 GreenNest. All rights reserved.”
-
-Navbar ও Footer সব রাউটে দৃশ্যমান থাকবে। রাউট পরিবর্তনে কোনো ক্র্যাশ বা রিলোড হবে না। */
-}
